4e459bd5688d76f09b79dfb223835a8d25f99f5d
nnkAV-debug <daysunlucky94@gmail.com>
2025-11-15 06:36:46 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nnk',
+  webDir: 'www'
+};
+
+export default config;
